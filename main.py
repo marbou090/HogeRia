33cda4734c2de86db05b6a526cdf3e4a7f502244
加藤まる <rikka2217@gmail.com>
2021-02-09 10:05:41 +0900

    NGワードリスト作った

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 import MeCab as MeeeeeeeCABBBBB
 import jaconv
+import pandas as pd
+import csv
+
 
 class GenerateRiaLang:
-
     def __init__(self):
         self.mecab = MeeeeeeeCABBBBB.Tagger()
+        with open('NGWord.csv') as f:
+            reader = csv.reader(f)
+            self.nglist = [row for row in reader]
 
     def changeria(self, text):
         thepart=''
@@ -16,19 +21,19 @@ class GenerateRiaLang:
             feature = line.split('\t')
             if len(feature) == 2: #'EOS'と''を省く
                 thepart = feature[1].split(',')
-        
-        print(thepart)
 
         if thepart[0] == '名詞':
             print('名詞りあだね。')
             return self.noun(text)
+        elif thepart[1] == '非自立' or thepart[0] == '助詞':
+            print('非自立語りあだね。')
+            return self.nonindepend(text,thepart[7])
         elif thepart[0] == '動詞':
-            if thepart[1] == '非自立':
-                print('非自立語りあだね。')
-                return self.verb(text.replace(jaconv.kata2hira(thepart[7]),''))
-            else:
-                print('動詞りあだね。')
-                return self.verb(text)
+            print('動詞りあだね。')
+            return self.verb(text)
+        elif thepart[0] == '助動詞':
+            print('助動詞りあだね。')
+            return self.auxverb(text,thepart[7])
         else:
             print(thepart[0]+'りあだね。')
             return (text + 'りあ')
@@ -40,6 +45,17 @@ class GenerateRiaLang:
     def verb(self, verb_text):
         verb_text = verb_text + 'りあ'
         return verb_text
+
+    def nonindepend(self, non_text,thepart):
+        non_text = non_text.replace(jaconv.kata2hira(thepart),'')
+        non_text = non_text + 'りあ'
+        return non_text
+
+    def auxverb(self, aux_text, thepart):
+        if (jaconv.kata2hira(thepart)) in self.nglist[0]:
+            aux_text = aux_text.replace(jaconv.kata2hira(thepart),'')
+        aux_text = aux_text + 'りあ'
+        return aux_text
 
 if __name__ == "__main__":
     ria = GenerateRiaLang()
