2b70753210613e17f29c466e1e1c3fbef2c311e8
marbou090 <marbou090@users.noreply.github.com>
2021-02-18 14:11:39 +0000

    deploy: 64dd899f71f7d4ed367e866dc7c9d5cb7d89b667

---
+++ _next/static/297nMK9KN8PV7bJP3UL7w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc7c0d911f90aa79996f4.css","static\u002Fchunks\u002Fpages\u002Findex-ca67ef193fe49851d3af.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8b902b8ef9845b459cd1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/gJRnMyVobBqyuiZuNE6uL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6f3109b1bac70376fd51.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9092a465b393c399ab6a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
